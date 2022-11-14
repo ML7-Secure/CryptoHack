6bdfc44aac4b899ff87bfa25aa81080b0f8e095b
ML7-Secure <yyt1776@live.fr>
2022-11-14 23:23:19 +0100

    crypto web

---
+++ 8.CryptoOnTheWeb/1.JWT/main.py
@@ -0,0 +1,11 @@
+import jwt
+
+def jwt_decode(encoded):
+    return jwt.decode(encoded, options={"verify_signature": False})
+
+def main():
+    encoded = "eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJmbGFnIjoiY3J5cHRve2p3dF9jb250ZW50c19jYW5fYmVfZWFzaWx5X3ZpZXdlZH0iLCJ1c2VyIjoiQ3J5cHRvIE1jSGFjayIsImV4cCI6MjAwNTAzMzQ5M30.shKSmZfgGVvd2OSB2CGezzJ3N6WAULo3w9zCl_T47KQ"
+    print( jwt_decode(encoded) )
+
+if __name__ == '__main__':
+    main()
